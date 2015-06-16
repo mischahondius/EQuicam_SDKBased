266ed1d6c786561bf584787c8eba1a4b6bfd62af
Mischa Hondius <mhondius@gmail.com>
2015-06-16 13:58:03 +0200

    camera invoerscherm, werkt nog niet

--- mediaPlayerSDKTest/src/main/java/veg/mediaplayer/sdk/test/Cameras.java
+++ mediaPlayerSDKTest/src/main/java/veg/mediaplayer/sdk/test/Cameras.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Context;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 import EQuicamApp.R;
 
@@ -12,22 +15,35 @@ import EQuicamApp.R;
  */
 public class Cameras extends Activity {
 
-    public static String currentCameraUrl;
-    private static final String defaultUrl = "rtsp://live:6mxNfzAG@equicam.noip.me:554/?inst=1/?audio_mode=0/?enableaudio=1/?h26x=4";
+    private static final String         defaultUrl = "rtsp://live:6mxNfzAG@equicam.noip.me:554/?inst=1/?audio_mode=0/?enableaudio=1/?h26x=4";
+    public static String                currentCameraUrl = defaultUrl;
+    private EditText                    cameraURLeditText;
+    private Button                      camerasSubmitBtn;
+    private EditText                    poortEditText;
 
     //Oncreate
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Camera's");
+        setContentView(R.layout.cameras);
+
+        //setkeyboard to appear directly
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
 
         //if empty
-        if (currentCameraUrl == null) {
+        if (currentCameraUrl.isEmpty()) {
             currentCameraUrl = defaultUrl;
         }
 
-        setContentView(R.layout.cameras);
+        //get buttons
+        cameraURLeditText = (EditText) findViewById(R.id.cameraURLeditText);
+        camerasSubmitBtn = (Button) findViewById(R.id.camerasSubmitBtn);
+        poortEditText = (EditText) findViewById(R.id.poortEditText);
 
+        //set hint current urls
+        cameraURLeditText.setHint("Bijv. equicam.noip.me");
+        poortEditText.setHint("Bijv. 554");
 
     }
 
@@ -35,7 +51,7 @@ public class Cameras extends Activity {
     public static String getCurrentCameraUrl(){
 
         //if empty
-        if (currentCameraUrl == null) {
+        if (currentCameraUrl.isEmpty()) {
             currentCameraUrl = defaultUrl;
         }
         return currentCameraUrl;
